128188803abc2874ee08f51da3b8f60b921a4120
Livni <llmc0383@gmail.com>
2023-07-10 09:36:19 -0600

    se agrego estilo

--- index.php
+++ index.php
@@ -4,14 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport">
     <title>Tarea_3</title>
+    <style>
+    body {
+        background-color: #e6e6fa;
+    }
+    
+    h1 {
+        color: #9370db;
+    }
+
+    input[type="text"] {
+        background-color: #d8bfd8; 
+        padding: 5px; 
+    }
+    
+    #output {
+        background-color: #e0b0ff; 
+        padding: 15px; 
+    }
+    
+    .div {
+        background-color: #e0b0ff; 
+        padding: 20px; 
+    }
+</style>
 </head>
 <body>
 <h1>Evento de Teclado</h1>
 
     <label for="textBox">Ingrese su información personal:</label>
     <input type="text" id="textBox">
-    <p id="output"></p>
-    
+    <div class="output-container">
+        <p id="output"></p>
+    </div>
     <script src="script.js"></script>
 </body>
 </html>
