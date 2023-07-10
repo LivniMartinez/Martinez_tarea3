d253a541b6a64566127ca72f1375523a9cdd3536
Livni <llmc0383@gmail.com>
2023-07-10 09:09:02 -0600

    se creo una caja de texto para crear un evento al teclado

--- index.php
+++ index.php
@@ -6,8 +6,12 @@
     <title>Tarea_3</title>
 </head>
 <body>
- 
-    <button>Click</button>
+<h1>Evento de Teclado</h1>
+
+    <label for="textBox">Ingrese su información personal:</label>
+    <input type="text" id="textBox">
+    <p id="output"></p>
+    
     <script src="script.js"></script>
 </body>
 </html>
