47c9d59cce04edd27460eb0952479e2c0e0e1b0b
Livni <llmc0383@gmail.com>
2023-07-10 09:03:28 -0600

    se crea el index para la tarea

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport">
+    <title>Tarea_3</title>
+</head>
+<body>
+ 
+    <button>Click</button>
+    <script src="script.js"></script>
+</body>
+</html>
